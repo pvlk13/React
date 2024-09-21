ef1a598b730e666a18cbfbeefcdf3854e78c4d37
vijayalakshmi <vijayalakshmi.kp@gmail.com>
2024-09-21 22:51:32 +0200

    tailwind

---
+++ 03tailwindprops/03tailwindprops/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-300 text-black p-4 rounded-xl mb-4">
+        Tailwind Css
+      </h1>
+      <Card userName="Vijaya" btnText="visit me" />
+    </>
+  );
+}
+
+export default App;
