82500fa80db5d61deafeeab5f67384d802369581
vijayalakshmi <vijayalakshmi.kp@gmail.com>
2024-10-01 22:01:29 +0200

    Login,Postcard and index components

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -2,8 +2,10 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button"
+import Input from "./Input"
 
 import Container from './Container/Container';
 
 
-export { Header, Footer, Logo, LogoutBtn, Container };
+export { Header, Footer, Logo, LogoutBtn, Container, Button, Input };
