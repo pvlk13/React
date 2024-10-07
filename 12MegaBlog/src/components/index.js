8d74931e2842c4f5e7358e484627fe44db7bba67
vijayalakshmi <vijayalakshmi.kp@gmail.com>
2024-10-07 23:01:46 +0200

    pages

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -6,8 +6,9 @@ import Button from "./Button"
 import Input from "./Input"
 import authService from "../appwrite/auth";
 import RTE from "./RTE";
-
 import Container from './Container/Container';
+import Signup from './Signup';
+import Login from './Login';
 
 
-export { Header, Footer, Logo, LogoutBtn, Container, Button, Input, RTE };
+export { Header, Footer, Logo, LogoutBtn, Container, Button, Input, RTE, Signup, Login };
