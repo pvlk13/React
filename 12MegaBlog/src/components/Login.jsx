167b4c0609535b76ea9f2fd55b5ea9daeba2c6ed
vijayalakshmi <vijayalakshmi.kp@gmail.com>
2024-10-12 12:45:15 +0200

    final project

--- 12MegaBlog/src/components/Login.jsx
+++ 12MegaBlog/src/components/Login.jsx
@@ -1,12 +1,10 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { login as authLogin } from "../store/authSlice";
+import { Button, Input, Logo } from "./index";
+import { useDispatch } from "react-redux";
 import authService from "../appwrite/auth";
 import { useForm } from "react-hook-form";
-import { Button, Input } from "./index";
-import { Link, useNavigate } from "react-router-dom";
-import React from "react";
-
 function Login() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
