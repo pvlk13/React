882e40e5c12ca650cd4f30f561b106f18f4d9ea4
vijayalakshmi <vijayalakshmi.kp@gmail.com>
2024-11-15 23:31:33 +0100

    CSS

--- 14techNews/src/components/Stories.jsx
+++ 14techNews/src/components/Stories.jsx
@@ -10,10 +10,26 @@ function Stories() {
       </div>
     );
   return (
-    <div>
-      {hits.map((item) => (
-        <li>{item.title}</li>
-      ))}
+    <div className="stories-div">
+      {hits.map((story) => {
+        const { title, url, author, num_comments, objectID } = story;
+        return (
+          <>
+            <div className="card">
+              <h2>{title}</h2>
+              <p>
+                By <span>{author}</span>| <span>{num_comments}</span> comments
+              </p>
+              <div className="card-button">
+                <a href={url} target="_blank">
+                  Read More{" "}
+                </a>
+                <a href="#">Remove</a>
+              </div>
+            </div>
+          </>
+        );
+      })}
     </div>
   );
 }
